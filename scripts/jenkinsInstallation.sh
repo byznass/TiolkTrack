861bb12219e95021edc3da023271f4a669eb208c
byznass-dev <byznass.group@gmail.com>
2019-02-02 14:56:29 +0200

    Added backup restoration

--- scripts/jenkinsInstallation.sh
+++ scripts/jenkinsInstallation.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-bucketName=''
-jenkinsDataFolder=''
+bucketName='tioltrack-backup'
+jenkinsDataFolder='/home/byznass_group/byznass/TiolkTrack/jenkinsci-byznass'
+
+###################################################################################
+
+backupName='jenkinsbackupLatest.tar.gz'
+
+mkdir -p "$jenkinsDataFolder"
+cd "$jenkinsDataFolder"
+
+gsutil cp $(gsutil ls gs://"$bucketName"/Jenkins-* | tail -1) "$backupName"
+tar -xf "$backupName"
+rm "$backupName"
 
 ####################################################################################
 
 wget https://raw.githubusercontent.com/byznass/jenkinsci-byznass/master/Dockerfile
-
 sudo docker build -t jenkinsci/byznass -f Dockerfile .
-
-sudo docker run -d --rm --name jenkinsci-byznass -u root -p 8080:8080 -p 50000:50000 -v /home/byznass_group/byznass/TiolkTrack/jenkinsci-byznass/data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkinsci/byznass
+sudo docker run -d --rm --name jenkinsci-byznass -u root -p 8080:8080 -p 50000:50000 -v "$jenkinsDataFolder"/data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jen
+kinsci/byznass
 
 ####################################################################################
 
 wget -O jenkinsBackUp.sh https://raw.githubusercontent.com/byznass/TiolkTrack/master/scripts/jenkinsBackUp.sh
-
 printf "#!/bin/bash\n\nbucketName='$bucketName'\njenkinsDataFolder='$jenkinsDataFolder'\n\n" | cat - jenkinsBackUp.sh > temp && mv temp jenkinsBackUp
-
 chmod +x jenkinsBackUp
-
-sudo cp jenkinsBackUp /etc/cron.daily/
+sudo mv jenkinsBackUp /etc/cron.daily/
+rm jenkinsBackUp.sh
