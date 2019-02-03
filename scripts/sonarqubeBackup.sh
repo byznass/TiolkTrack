4968814d3c6b916249dd849ac813ffc2867260e6
byznass-dev <byznass.group@gmail.com>
2019-02-03 17:40:06 +0200

    Created the script for backup-ing sonarqube data

---
+++ scripts/sonarqubeBackup.sh
@@ -0,0 +1,17 @@
+# !!! in /etc/cron.daily this script need to be without ".sh" extension 
+
+sonarqubeDataFolder=''
+bucketName=''
+
+cd "$sonarqubeDataFolder"
+
+dateString=`date +%Y-%m-%d-%H:%M`
+
+sudo tar c TiolkTrack/ | gzip --best > /tmp/SonarQube-"$dateString".tar.gz
+
+gsutil cp  /tmp/SonarQube-"$dateString".tar.gz gs://"$bucketName"
+
+gsutil ls gs://"$bucketName"/SonarQube-* | tac | tail +6 | xargs gsutil rm
+#gsutil ls -l gs://"$bucketName"/SonarQube-* | tac | tail +2 | sort -k 2 | awk '{print $3}' | tail +6 | xargs gsutil rm
+
+rm  /tmp/SonarQube-"$dateString".tar.gz
