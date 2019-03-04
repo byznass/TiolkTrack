b6ad14effe34b6a342f04fc4910befb82a7a68c0
byznass-dev <byznass.group@gmail.com>
2019-03-04 23:15:05 +0200

    A commit with issues

--- src/main/java/MockClass.java
+++ src/main/java/MockClass.java
@@ -1,7 +1,24 @@
+import com.sun.istack.internal.Nullable;
+
+import java.util.Optional;
+
 public class MockClass {
 
     public int doSomething(int seed) {
 
         return seed + 5;
+    }
+
+    public void doSomething () {
+        Optional<String> optional = getOptional();
+        if (optional != null) {  // Noncompliant
+            // do something with optional...
+        }
+    }
+
+    @Nullable // Noncompliant
+    public Optional<String> getOptional() {
+        // ...
+        return null;  // Noncompliant
     }
 }
