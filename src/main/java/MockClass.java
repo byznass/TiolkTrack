22a9efb3d5bb591f044105e5faea4c0d2c491832
byznass-dev <byznass.group@gmail.com>
2019-03-12 00:42:42 +0200

    Deleted unnecessary code

--- src/main/java/MockClass.java
+++ src/main/java/MockClass.java
@@ -1,21 +1,8 @@
-import java.util.Optional;
 
 public class MockClass {
 
     public int doSomething(int seed) {
 
         return seed + 5;
-    }
-
-    public void doSomething () {
-        Optional<String> optional = getOptional();
-        if (optional != null) {  // Noncompliant
-            // do something with optional...
-        }
-    }
-
-    public Optional<String> getOptional() {
-        // ...
-        return null;  // Noncompliant
     }
 }
