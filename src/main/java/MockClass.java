aca22ee945bbaaed19537d16352930187df71717
byznass-dev <byznass.group@gmail.com>
2019-03-16 16:25:14 +0200

    Resolved blocker issue

--- src/main/java/MockClass.java
+++ src/main/java/MockClass.java
@@ -5,27 +5,4 @@ public class MockClass {
 
         return seed + 5;
     }
-
-    public static void main(String[] args) {
-
-        Day day = null;
-
-        switch (day) {
-            case MONDAY:
-            case TUESDAY:
-                WEDNESDAY:   // Noncompliant; syntactically correct, but behavior is not what's expected
-                doSomething();
-                break;
-        }
-    }
-
-    private static void doSomething() {
-
-    }
-
-    private enum Day {
-        MONDAY,
-        TUESDAY,
-        WEDNESDAY
-    }
 }
