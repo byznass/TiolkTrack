fc02650d3d773b50c0ff21f063923a3d775d39d9
byznass-dev <byznass.group@gmail.com>
2019-04-27 12:37:32 +0300

    TT-33 minor changes

--- src/main/java/com/byznass/tiolktrack/kernel/util/IdentifierValidator.java
+++ src/main/java/com/byznass/tiolktrack/kernel/util/IdentifierValidator.java
@@ -5,6 +5,10 @@ public class IdentifierValidator {
 	public static final int MAX_LENGTH = 50;
 	public static final String CHAR_SET = "latin letters, digits, underscore, dash or dot characters";
 
+	private IdentifierValidator() {
+
+	}
+
 	public static boolean validate(String identifier) {
 
 		return identifier.matches("^[a-zA-Z0-9_.\\-]+") && identifier.length() <= MAX_LENGTH;
