f35074ad2a3f5a46ab63602013c048dd98735364
byznass-dev <byznass.group@gmail.com>
2019-04-30 00:27:15 +0300

    TT-34 broke an integration test

--- src/dbIntegrationTest/java/com/byznass/tiolktrack/postgres/dao/PostgresUserProviderIntegrationTest.java
+++ src/dbIntegrationTest/java/com/byznass/tiolktrack/postgres/dao/PostgresUserProviderIntegrationTest.java
@@ -42,7 +42,7 @@ public class PostgresUserProviderIntegrationTest extends DatabaseIntegrationTest
 		try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 			preparedStatement.setString(1, user.getId());
 			preparedStatement.setBytes(2, user.getPassHash());
-			preparedStatement.setBytes(3, user.getPassSalt());
+			preparedStatement.setBytes(3, user.getPassHash());
 
 			int numberOfAffectedRows = preparedStatement.executeUpdate();
 
